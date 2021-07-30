5b791738d45d4d1d0ffdd02e9ca9ebd41905a53e
tanvirahmed707 <tanvirahmed50516@gmail.com>
2021-07-30 21:49:06 +0600

    Labtask7

---
+++ Labtask3/Login.php
@@ -0,0 +1,46 @@
+<!DOCTYPE HTML>
+<html>  
+<body>
+
+
+
+
+<?php
+
+$name = $password = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $name = test_input($_POST["name"]);
+  $password = test_input($_POST["password"]);
+  
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+?>
+
+<h2>LogIn</h2>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
+  Name: <input type="text" name="name">
+  <br><br>
+  Password: <input type="text" name="password">
+  <br><br>
+  <input type="submit" name="submit" value="Submit">  
+
+
+
+
+
+
+
+ 
+
+ 
+
+  </form>
+</body>
+</html
---
+++ Labtask3/Profile picture.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+
+
+<form action="upload.php" method="post" enctype="multipart/form-data">
+  Profile Picture:
+  <input type="file" name="fileToUpload" id="fileToUpload">
+  <input type="submit" value="Upload Image" name="submit">
+</form>
+
+</body>
+</html>

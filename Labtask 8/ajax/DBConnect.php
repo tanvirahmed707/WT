3723e68fc4215abcbb320bb17c1f1c38068e0ecf
tanvirahmed707 <75059110+tanvirahmed707@users.noreply.github.com>
2021-08-13 21:26:01 +0600

    Delete Labtask 8/ajax directory

--- Labtask 8/ajax/DBConnect.php
+++
@@ -1,9 +0,0 @@
-<?php
-  function connect() {
-    $conn = new mysqli("localhost", "Tanvir", "12345", "wkt");
-    if ($conn->connect_errno) {
-      die("failed to connect");
-    }
-    return $conn;
-  }
-?>
